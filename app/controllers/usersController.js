c112bb4346dbdea0aa517071fe3ad781e96af3b8
greedybrain <greedybrain18@gmail.com>
2020-11-17 09:42:53 -0500

    abstracted away try catch blocks for each action

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -6,87 +6,79 @@ const _ = require('lodash')
 const { User, validateUser } = require('../models/user')
 const { validateNote } = require('../models/note')
 
+//! handle errors
+// const asyncMiddleware = handler => {
+//         return async (req, res, next) => {
+//                 try {
+//                         await handler(req, res)
+//                 } catch (exc) {
+//                         next(exc)
+//                 }
+//         }
+// } => then wrap each 'route/action' in this ^ method
 
 const getCurrentUser = async (req, res) => {
-        try {
-                const user = await User.findById(req.user._id) .select('-password -createdAt -updatedAt -__v')
-                if (!user) return res.status(404).send("User does not exist")
-                res.send(user)
-        } catch(exc) {
-                console.log(exc.message)
-        }
+        const user = await User.findById(req.user._id) .select('-password -createdAt -updatedAt -__v')
+        if (!user) return res.status(404).send("User does not exist")
+        res.send(user)
 }
 
 const signupUser = async (req, res) => {
-        const { error } = validateUser(req.body)
-        if(error) return res.status(400).send(error.message)
-
+        validateUser(req.body, res)
+        
         let user = await User.findOne({ email: req.body.email })
         if(user) return res.status(400).send("User already exists!")
 
         user = new User(_.pick(req.body, ['name', 'email', 'password']))
         const salt = await bcrypt.genSalt(10)
         user.password = await bcrypt.hash(user.password, salt)
-        
-        try {
-                user = await user.save()
-                const token = user.generateAuthToken()
-                res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
-        } catch (exc) {
-                for (field in exc.errors) {
-                        console.error(exc.errors[field].message)
-                }
-        }
+
+        user = await user.save()
+        const token = user.generateAuthToken()
+        res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
 }
 
 const getCurrentUsersNotes = async (req, res) => {
         const user = await User.findById(req.user._id).select('-password')
-
         const notes = user.notes.sort((a, b) => a.title.localeCompare(b.title))
         res.send(notes)
 }
 
+
 const createNote = async (req, res) => {
-        const { error } = validateNote(req.body)
-        if (error) return res.status(400).send("Check the title or body content of your note")
+        validateNote(req.body, res)
 
         let user = await User.findById(req.user._id)
-        user.notes.push(_.pick(req.body, ['title', 'content']))
-        try {
-                user = await user.save()
-                res.send(user)
-        } catch (exc) {
-                for (field in exc.errors) console.error(exc.errors[field].message)
-        }
+        const note = _.pick(req.body, ['title', 'content'])
+        user.notes.push(note)
+
+        user = await user.save()
+        res.send(user)
 }
+
 
 const updateNote = async (req, res) => {
-        const { error } = validateNote(req.body)
-        if (error) return res.status(400).send("Check the title or body content of your note")
-
+        validateNote(req.body, res)
+        
         const user = await User.findById(req.user._id)
-        let note = user.notes.id(req.params.note_id)
+        const note = user.notes.id(req.params.note_id)
         note.title = req.body.title 
         note.content = req.body.content 
-        try {
-                await user.save()
-                res.send(note)
-        } catch (error) {
-                console.log(error)
-        }
+
+        await user.save()
+        res.send(note)
 }
+
 
 const deleteNote = async (req, res) => {
         const user = await User.findById(req.user._id)
         const note = user.notes.id(req.params.note_id)
-        try {
-                await note.remove()
-                await user.save()
-                res.send(note)
-        } catch (error) {
-                console.log(error)
-        }
+
+        await note.remove()
+        await user.save()
+        res.send(note)
 }
+
 
 //! Exports
 module.exports = {
