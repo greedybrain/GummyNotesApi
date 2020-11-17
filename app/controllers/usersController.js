cba7ddbf419324519325f1789a78ffe77bc7f6f2
greedybrain <greedybrain18@gmail.com>
2020-11-16 22:51:00 -0500

    added notes actions in usersController

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -4,16 +4,17 @@ const _ = require('lodash')
 
 //! Custom
 const { User, validateUser } = require('../models/user')
+const { validateNote } = require('../models/note')
 
-const getUser = async (req, res) => {
-        const { error } = validateUser(req.body)
-        if(error) return res.status(400).send("Invalid email or password")
 
-        let user = await User.findById(req.params.id)
-        if (!user) return res.status(400).send("Invalid user")
-
-        user = _.pick(user, ['_id', 'name', 'email'])
-        res.send(user)
+const getCurrentUser = async (req, res) => {
+        try {
+                const user = await User.findById(req.user._id) .select('-password -createdAt -updatedAt -__v')
+                if (!user) return res.status(404).send("User does not exist")
+                res.send(user)
+        } catch(exc) {
+                console.log(exc.message)
+        }
 }
 
 const signupUser = async (req, res) => {
@@ -36,10 +37,63 @@ const signupUser = async (req, res) => {
                         console.error(exc.errors[field].message)
                 }
         }
-
 }
 
+const getCurrentUsersNotes = async (req, res) => {
+        const user = await User.findById(req.user._id).select('-password')
+
+        const notes = user.notes.sort((a, b) => a.title.localeCompare(b.title))
+        res.send(notes)
+}
+
+const createNote = async (req, res) => {
+        const { error } = validateNote(req.body)
+        if (error) return res.status(400).send("Check the title or body content of your note")
+
+        let user = await User.findById(req.user._id)
+        user.notes.push(_.pick(req.body, ['title', 'content']))
+        try {
+                user = await user.save()
+                res.send(user)
+        } catch (exc) {
+                for (field in exc.errors) console.error(exc.errors[field].message)
+        }
+}
+
+const updateNote = async (req, res) => {
+        const { error } = validateNote(req.body)
+        if (error) return res.status(400).send("Check the title or body content of your note")
+
+        const user = await User.findById(req.user._id)
+        let note = user.notes.id(req.params.note_id)
+        note.title = req.body.title 
+        note.content = req.body.content 
+        try {
+                await user.save()
+                res.send(note)
+        } catch (error) {
+                console.log(error)
+        }
+}
+
+const deleteNote = async (req, res) => {
+        const user = await User.findById(req.user._id)
+        const note = user.notes.id(req.params.note_id)
+        try {
+                await note.remove()
+                await user.save()
+                res.send(note)
+        } catch (error) {
+                console.log(error)
+        }
+}
+
+//! Exports
 module.exports = {
+        getCurrentUser,
         signupUser,
-        getUser
+        getCurrentUsersNotes,
+        createNote,
+        updateNote,
+        deleteNote,
 }
