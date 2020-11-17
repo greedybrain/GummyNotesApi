8c0c8088369297c9153f0f5ca819c169d8270283
greedybrain <greedybrain18@gmail.com>
2020-11-16 22:53:05 -0500

    imported note actions to be used in user routes

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,12 +3,23 @@ const express = require('express')
 const userRouter = express.Router()
 
 //! Custom
-const { signupUser, getUser } = require('../controllers/usersController')
-
+const { 
+        signupUser, 
+        getCurrentUser, 
+        getCurrentUsersNotes, 
+        createNote,
+        updateNote,
+        deleteNote,
+} = require('../controllers/usersController')
+const { ifAuthorized } = require('../middleware/auth')
 
 //! Define routes
-userRouter.get('/:id', async (req, res) => await getUser(req, res))
+userRouter.get('/me', ifAuthorized, async (req, res) => await getCurrentUser(req, res))
 userRouter.post('/', async (req, res) => await signupUser(req, res))
+userRouter.get('/:id/notes', ifAuthorized, async (req, res) => await getCurrentUsersNotes(req, res))
+userRouter.post('/:id/notes', ifAuthorized, async (req, res) => await createNote(req, res))
+userRouter.put('/:user_id/notes/:note_id', ifAuthorized, async (req, res) => await updateNote(req, res))
+userRouter.delete('/:user_id/notes/:note_id', ifAuthorized, async (req, res) => await deleteNote(req, res))
 
 //! Exporting 
 module.exports = userRouter
