5b41c7b9a3d0e4abdd34556adc655e2b17abcc0c
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:02:04 -0500

    rafacd login helper function to use passport

--- app/controllers/authsController.js
+++ app/controllers/authsController.js
@@ -2,29 +2,54 @@
 const { User } = require('../models/user')
 
 //! Packages
+const passport = require('passport');
 const _ = require('lodash')
 const Joi = require('joi')
 const bcrypt = require('bcrypt')
 
-const loginUser = async (req, res) => {
-        // Check for errors client side
+// const loginUser = async (req, res) => {
+//         // Check for errors client side
+//         const { error } = validate(req.body)
+//         if (error) return res.status(400).send(error.message)
+
+//         // Check if user exists and is valid
+//         let user = await User.findOne({ email: req.body.email })
+//         if(!user) return res.status(400).send("Invalid email or password")
+
+//         /*
+//         Use lodash pick method to pick the props we want to use from req.body 
+//         Grab the name email password, then check to see if the password matches
+//         the hashed version using bcrypt
+//         */
+//         const isValidPassword = await bcrypt.compare(req.body.password, user.password)
+//         if (!isValidPassword) return res.status(400).send("Invalid email or password")
+
+//         const token = user.generateAuthToken()
+//         res.header('x-auth-token', token).send({
+//                 token,
+//                 user: _.pick(user, ['_id', 'name', 'email'])
+//         })
+// }
+
+const loginUser = async (req, res, next) => {
         const { error } = validate(req.body)
         if (error) return res.status(400).send(error.message)
 
-        // Check if user exists and is valid
-        let user = await User.findOne({ email: req.body.email })
-        if(!user) return res.status(400).send("Invalid email or password")
+        passport.authenticate('local', (err, user, info) => {
+                if (err) return next(err);
+                if (!user) return res.status(400).send("Invalid user")
+                req.logIn(user, err => {
+                        if (err) return next(err); 
+                        user = _.pick(user, ['name', 'email', 'notes'])
+                        res.send(user)
+                });
+        })(req, res, next);
+}
 
-        /*
-        Use lodash pick method to pick the props we want to use from req.body 
-        Grab the name email password, then check to see if the password matches
-        the hashed version using bcrypt
-        */
-        const isValidPassword = await bcrypt.compare(req.body.password, user.password)
-        if (!isValidPassword) return res.status(400).send("Invalid email or password")
-
-        const token = user.generateAuthToken()
-        res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
+const logoutUser = async (req, res) => {
+        req.logout()
+        req.session.cookie.expires = Date.now()
+        res.send('Logged out successfully')
 }
 
 //! Validations
@@ -37,5 +62,6 @@ const validate = req => {
 }
 
 module.exports = {
-        loginUser
+        loginUser,
+        logoutUser
 }
