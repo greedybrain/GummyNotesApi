5067cf1805bbe5d5a76dacc781e98c4ab87a1d40
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:01:47 -0500

    added cors and session npm

--- index.js
+++ index.js
@@ -2,19 +2,26 @@
 require('express-async-errors')
 const config = require('config')
 const mongoose = require('mongoose')
+const cors = require('cors');
+const passport = require('passport');
+// const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const express = require('express')
+
 const app = express()
 
 //! Custom 
 // const { handleErrors } = require('./app/middleware/error')
+const { configuredLocalStrategy } = require('./config/passportConfig');
+configuredLocalStrategy(passport)
 const auth = require('./app/routes/auths')
-const users = require('./app/routes/users')
+const users = require('./app/routes/users');
 
 
-if (!config.get('jwtPrivateKey')) {
-        console.log("FATAL ERROR: jwtPrivateKey is not defined.")
-        process.exit(1)
-}
+// if (!config.get('jwtPrivateKey')) {
+//         console.log("FATAL ERROR: jwtPrivateKey is not defined.")
+//         process.exit(1)
+// }
 
 //! Connecting to database
 const connectionString = config.get('connection_string')
@@ -30,12 +37,27 @@ mongoose.connect(connectionString, {
 
 //! Add middleware 
 app.use(express.json())
-app.use('/api/auth', auth)
-app.use('/api/users', users)
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+        origin: 'http://localhost:3000', // react connection,
+        credentials: true
+}))
+app.use(session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
 // app.use(handleErrors)
 
 //! Listening 
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 app.listen(PORT, error => {
         if(error) console.error(error)       
         console.log(`Listening on PORT ${PORT}`)
