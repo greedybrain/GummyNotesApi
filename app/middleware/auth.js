e28fe6f6708f67959f137b65c577b034d247a153
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:03:06 -0500

    changed ifAuthorized method

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,18 +1,23 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+// const jwt = require('jsonwebtoken')
+// const config = require('config')
+
+// const ifAuthorized = (req, res, next) => {
+//         const token = req.header('x-auth-token')
+//         if(!token) return res.status(401).send("Access denied. No token provided")
+        
+//         try {
+//                 const decodedPayload = jwt.verify(token, config.get('jwtPrivateKey'))
+//                 req.user = decodedPayload
+//                 next()
+
+//         } catch (error) {
+//                 res.status(400).send("Invalid token")
+//         }
+// }
 
 const ifAuthorized = (req, res, next) => {
-        const token = req.header('x-auth-token')
-        if(!token) return res.status(401).send("Access denied. No token provided")
-        
-        try {
-                const decodedPayload = jwt.verify(token, config.get('jwtPrivateKey'))
-                req.user = decodedPayload
-                next()
-
-        } catch (error) {
-                res.status(400).send("Invalid token")
-        }
+        if (!req.isAuthenticated()) return res.status(400).send("You must be logged in to do that")
+        return next()
 }
 
 module.exports = {
