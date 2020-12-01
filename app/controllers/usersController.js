4ce1693e0b50001195d9e9f0e8264944a82ee6e3
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:02:27 -0500

    refacd signup helper function

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -19,24 +19,42 @@ const { validateNote } = require('../models/note')
 
 //! Helper functions
 const getCurrentUser = async (req, res) => {
-        const user = await User.findById(req.user._id) .select('-password -createdAt -updatedAt -__v')
+        let user = await User.findById(req.user._id) .select('-password -createdAt -updatedAt -__v')
         if (!user) return res.status(404).send("User does not exist")
+
+        user = _.pick(user, ['name', 'email', 'notes'])
+
         res.send(user)
 }
 
-const signupUser = async (req, res) => {
-        validateUser(req.body, res)
+// const signupUser = async (req, res) => {
+        // validateUser(req.body, res)
         
+        // let user = await User.findOne({ email: req.body.email })
+        // if(user) return res.status(400).send("User already exists!")
+
+        // user = new User(_.pick(req.body, ['name', 'email', 'password', 'confirmPassword']))
+        // const salt = await bcrypt.genSalt(10)
+        // user.password = await bcrypt.hash(user.password, salt)
+        // user.confirmPassword = await bcrypt.hash(user.password, salt)
+
+        // user = await user.save()
+        // const token = user.generateAuthToken()
+        // res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
+// }
+
+const signupUser = async (req, res) => {
+        const { error } = validateUser(req.body)
+        if(error) return res.status(400).send(error.message)
+
         let user = await User.findOne({ email: req.body.email })
         if(user) return res.status(400).send("User already exists!")
 
-        user = new User(_.pick(req.body, ['name', 'email', 'password']))
-        const salt = await bcrypt.genSalt(10)
-        user.password = await bcrypt.hash(user.password, salt)
-
+        user = new User(_.pick(req.body, ['name', 'email', 'password', 'confirmPassword']))
+        user.password = await bcrypt.hash(req.body.password, 10)
+        user.confirmPassword = await bcrypt.hash(req.body.confirmPassword, 10)
         user = await user.save()
-        const token = user.generateAuthToken()
-        res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
+        res.send(user)
 }
 
 const getCurrentUsersNotes = async (req, res) => {
