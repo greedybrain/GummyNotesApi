9e74c37244e39381495b23a1de9051476c0b7568
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:05:08 -0500

    added logout method to routes

--- app/routes/auths.js
+++ app/routes/auths.js
@@ -3,10 +3,11 @@ const express = require('express');
 const authRouter = express.Router()
 
 //! Custom
-const { loginUser } = require('../controllers/authsController')
+const { loginUser, logoutUser } = require('../controllers/authsController')
 
 //! Create login request
 authRouter.post('/', async (req, res) => await loginUser(req, res))
+authRouter.delete('/logout', async (req, res) => await logoutUser(req, res))
 
 //! Export router to use in index.js file 
 module.exports = authRouter
