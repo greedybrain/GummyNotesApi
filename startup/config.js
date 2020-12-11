d7de7f2e7989ca033eabe45399d4e07ce59b721a
greedybrain <greedybrain18@gmail.com>
2020-12-11 11:46:16 -0500

    ...

--- startup/config.js
+++ startup/config.js
@@ -11,6 +11,7 @@ module.exports = function(app, express, config, passport) {
 
         app.use(cors({
                 origin: 'https://gifted-dubinsky-612231.netlify.app', // react connection,
+                methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
                 allowedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
                 credentials: true
         }))
--- startup/dist/config.dev.js
+++ startup/dist/config.dev.js
@@ -20,6 +20,7 @@ module.exports = function (app, express, config, passport) {
   app.use(cors({
     origin: 'https://gifted-dubinsky-612231.netlify.app',
     // react connection,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
     credentials: true
   }));
