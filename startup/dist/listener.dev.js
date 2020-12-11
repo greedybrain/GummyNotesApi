e667001071a49c1fc0283938442889c961377844
greedybrain <greedybrain18@gmail.com>
2020-12-11 10:18:20 -0500

    ...

--- startup/dist/listener.dev.js
+++ startup/dist/listener.dev.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function (app) {
-  var PORT = process.env.PORT === undefined ? 5000 : 5000;
+  var PORT = process.env.PORT || 5000;
   app.listen(PORT, function (error) {
     if (error) console.error(error);
     console.log("Listening on PORT ".concat(PORT));
--- startup/listener.js
+++ startup/listener.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-        const PORT = process.env.PORT === undefined ? 5000 : 5000
+        const PORT = process.env.PORT || 5000
         app.listen(PORT, error => {
                 if(error) console.error(error)       
                 console.log(`Listening on PORT ${PORT}`)
